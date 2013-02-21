d2c550ad961fc77e2836712a20c479243764b3e0
Jonathan Martin <me@nybblr.com>
2013-02-21 17:59:33 -0500

    Added start state and accepts.

--- src/NFA.java
+++ src/NFA.java
@@ -3,4 +3,5 @@ import java.util.ArrayList;
 public class NFA {
 	public ArrayList<Transition> transitions = new ArrayList<Transition>();
 	public ArrayList<State> states = new ArrayList<State>();
+	public State start;
 }
--- src/State.java
+++ src/State.java
@@ -3,4 +3,5 @@ import java.util.ArrayList;
 public class State {
 	public String label;
 	public ArrayList<Transition> transitions = new ArrayList<Transition>();
+	public Boolean accepts;
 }
