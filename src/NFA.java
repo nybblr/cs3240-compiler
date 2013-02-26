c1fbf86ebf121af9e5372b6de495c305494b5234
Jonathan Martin <me@nybblr.com>
2013-02-26 16:35:47 -0500

    Setters/getters and utility stubs.

--- src/NFA.java
+++ src/NFA.java
@@ -4,4 +4,20 @@ public class NFA {
 	public ArrayList<Transition> transitions = new ArrayList<Transition>();
 	public ArrayList<State> states = new ArrayList<State>();
 	public State start;
+
+	public State getStart() {
+		return start;
+	}
+
+	public void setStart(State start) {
+		this.start = start;
+	}
+
+	public ArrayList<Transition> getTransitions() {
+		return transitions;
+	}
+
+	public ArrayList<State> getStates() {
+		return states;
+	}
 }
--- src/State.java
+++ src/State.java
@@ -1,7 +1,41 @@
 import java.util.ArrayList;
 
 public class State {
-	public String label;
-	public ArrayList<Transition> transitions = new ArrayList<Transition>();
-	public Boolean accepts;
+	private String label;
+	private ArrayList<Transition> transitions = new ArrayList<Transition>();
+	private Boolean accepts;
+
+	/* Getters and setters */
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public ArrayList<Transition> getTransitions() {
+		return transitions;
+	}
+
+	public Boolean getAccepts() {
+		return accepts;
+	}
+
+	public void setAccepts(Boolean accepts) {
+		this.accepts = accepts;
+	}
+
+	/* Transitions */
+	public Boolean hasTransition(Character on, State to) {
+		return false;
+	}
+
+	public Boolean addTransition(Character on, State to) {
+		return false;
+	}
+
+	public Boolean deleteTransition(Character on, State to) {
+		return false;
+	}
 }
